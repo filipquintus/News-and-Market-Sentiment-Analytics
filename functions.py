7914c05ced3253666318f25df415e388bd0a4afc
Filip Willesen <filipquintus@hotmail.com>
2024-12-17 12:02:47 +0100

    hshshd

--- functions.py
+++ functions.py
@@ -2,30 +2,30 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-
+# Function for W2V version
 def find_similar_embedding_W2V(word1, word2, embedded_words, embeddings, W2V_embedding):
     embedding1 = W2V_embedding[word1]
     embedding2 = W2V_embedding[word2]
 
-
-    # Combine embeddings with normalization
+    # Combining embeddings
     combined_embedding = embedding1 + embedding2
 
-    # Compute cosine similarities with filtered embeddings
+    # Computing cosine distance to embedded wordlist
     similarities = cosine_similarity([combined_embedding], embeddings)
 
-    # Sort indices to get top 10 matches
+    # gets the top 10 matches
     sorted_indices = np.argsort(similarities[0])[::-1][:10]  # Top 10 matches
 
     # Retrieve the top words and their corresponding similarity scores
     top_words = [embedded_words[i] for i in sorted_indices]
     top_similarities = [similarities[0][i] for i in sorted_indices]
 
-    # Print results with cosine similarity
+    # Print top 10 closest words
     print(f"Top 10 closest words with cosine similarity for the sum of {word1} and {word2}:")
     for word, similarity in zip(top_words, top_similarities):
         print(f"{word}: {similarity:.4f}")
 
+    # Selects the closest not-input word
     if top_words[0] not in [word1, word2]:
         top_word = top_words[0]
     elif top_words[1] not in [word1, word2]:
@@ -36,27 +36,29 @@ def find_similar_embedding_W2V(word1, word2, embedded_words, embeddings, W2V_emb
 
     return top_word
 
-
+#Function for ST-version
 def find_similar_embedding_ST(word1, word2, embedded_words, embeddings, ST_embedding):
 
+    #Creating combined string
     combined_sentence = word1 + " and " + word2 + " combined"
     combined_embedding = ST_embedding.encode(combined_sentence)
 
-    # Compute cosine similarities with filtered embeddings
+    # Computing cosine distance to embedded wordlist
     similarities = cosine_similarity([combined_embedding], embeddings)
 
-    # Sort indices to get top 10 matches
+    # gets the top 10 matches
     sorted_indices = np.argsort(similarities[0])[::-1][:10]  # Top 10 matches
 
     # Retrieve the top words and their corresponding similarity scores
     top_words = [embedded_words[i] for i in sorted_indices]
     top_similarities = [similarities[0][i] for i in sorted_indices]
 
-    # Print results with cosine similarity
+    # Print top 10 closest words
     print(f'Top 10 closest words for "{word1} combined with {word2}" cosine similarity:')
     for word, similarity in zip(top_words, top_similarities):
         print(f"{word}: {similarity:.4f}")
 
+    # Selects the closest not-input word
     if top_words[0] not in [word1, word2]:
         top_word = top_words[0]
     elif top_words[1] not in [word1, word2]:
